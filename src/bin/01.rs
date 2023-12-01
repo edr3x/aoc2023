8438747b6fa1f7c652bc6af234f406d47a4ec211
edr3x <dhunganaannanta@gmail.com>
2023-12-02 00:59:44 +0545

    feat: day1 first part done

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,13 +1,39 @@
 mod reader;
 
 fn main() {
-    let file = reader::read_file("day01/example.txt");
+    let file = reader::read_file("day01/input.txt");
     part1(&file);
     part2(&file);
 }
 
 fn part1(file_data: &str) {
-    println!("part1: {}", file_data);
+    let data = file_data
+        .split('\n')
+        .filter(|x| !x.is_empty())
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|&x| {
+            x.chars()
+                .filter(|&c| c.is_ascii_digit())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let first = data
+        .iter()
+        .map(|x| x.first().expect(""))
+        .collect::<Vec<_>>();
+    let last = data.iter().map(|x| x.last().expect("")).collect::<Vec<_>>();
+
+    let res: i32 = first
+        .iter()
+        .zip(last.iter())
+        .map(|(&f, &l)| format!("{}{}", f, l).parse().expect("failed to parse"))
+        .collect::<Vec<_>>()
+        .iter()
+        .sum();
+
+    println!("Total:{}", res);
 }
 
 fn part2(file_data: &str) {
