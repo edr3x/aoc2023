720e85a6fcbf09b8cba0a6c6244965ef9734ed4b
edr3x <dhunganaannanta@gmail.com>
2023-12-02 14:11:33 +0545

    feat: added tests

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,12 +1,12 @@
 mod reader;
 
 fn main() {
-    let file = reader::read_file("day01/input.txt");
-    part1(&file);
-    part2(&file);
+    let file = reader::read_file("01.txt");
+    println!("Part1 answer: {}", part1(&file));
+    println!("Part2 answer: {}", part2(&file));
 }
 
-fn part1(file_data: &str) {
+fn part1(file_data: &str) -> i32 {
     let data = file_data
         .lines()
         .collect::<Vec<&str>>()
@@ -24,32 +24,29 @@ fn part1(file_data: &str) {
         .collect::<Vec<_>>();
     let last = data.iter().map(|x| x.last().expect("")).collect::<Vec<_>>();
 
-    let res: i32 = first
+    first
         .iter()
         .zip(last.iter())
         .map(|(&f, &l)| format!("{}{}", f, l).parse().expect("failed to parse"))
         .collect::<Vec<_>>()
         .iter()
-        .sum();
-
-    println!("part1 result: {}", res);
+        .sum()
 }
 
-fn part2(file_data: &str) {
+fn part2(file_data: &str) -> i32 {
     let mut res = 0;
     for line in file_data.lines() {
         let nums = extract_numbers(line);
         res += nums[0] * 10 + nums[1];
     }
-
-    println!("part2 result: {}", res);
+    res
 }
 
 const NUMBERS: [&str; 9] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
-fn extract_numbers(n: &str) -> [u32; 2] {
+fn extract_numbers(n: &str) -> [i32; 2] {
     let mut first = None;
     let mut last = 0;
 
@@ -61,15 +58,45 @@ fn extract_numbers(n: &str) -> [u32; 2] {
     let chars = n.as_bytes();
     for (i, c) in chars.iter().enumerate() {
         if c.is_ascii_digit() {
-            digit((c - b'0') as u32);
+            digit((c - b'0') as i32);
         } else {
             for (j, d) in NUMBERS.iter().enumerate() {
                 if chars[i..].starts_with(d.as_bytes()) {
-                    digit(j as u32 + 1);
+                    digit(j as i32 + 1);
                 }
             }
         }
     }
 
     [first.unwrap(), last]
+}
+
+#[cfg(test)]
+mod test {
+    const EXAMPLE_A: &str = indoc::indoc! {"
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+        "};
+
+    const EXAMPLE_B: &str = indoc::indoc! {"
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        "};
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(super::part1(EXAMPLE_A), 142);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(super::part2(EXAMPLE_B), 281);
+    }
 }
